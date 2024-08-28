bc1013dd3b6ab4e45c942e3320090ff050d1b370
divyasanvi1 <sanvidiv22@gmail.com>
2024-08-29 03:14:44 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import './App.css'
+import './App.css';
 import authService from './appwrite/auth';
-import {login,logout} from './store/authSlice'
+import {login,logout} from './store/authSlice';
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 function App() {
   const [loading,setLoading]=useState(true)
@@ -27,7 +29,13 @@ function App() {
 
    return !loading ? (
    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
-
+    <div className='w-full block'>
+     <Header/>
+     <main>
+      {/* {<Outlet/>} */}
+     </main>
+     <Footer/>
+    </div>
    </div>)
    : null
 }
