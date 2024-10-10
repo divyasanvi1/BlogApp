974f2ace7e7c1a49e96d2c9ca170945e0de5d3cd
divyasanvi1 <sanvidiv22@gmail.com>
2024-10-10 08:51:56 +0530

    pages

--- src/components/index.js
+++ src/components/index.js
@@ -3,10 +3,24 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn"
+import Input from "./Input";
+import RTE from "./RTE";
+import SignUp from "./Signup";
+import Login from "./Login";
+import PostForm from "./Post-Form/PostForm";
+import PostCard from "./PostCard"
+import AuthLayout from "./AuthLayout"
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Input,
+    RTE,
+    SignUp,
+    Login,
+    PostForm,
+    AuthLayout,
+    PostCard
 }
